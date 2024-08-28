b7e6135298fba21f0b0a3a8471e8c2296f656f52
Harjot <144142200+harjot38@users.noreply.github.com>
2024-08-28 08:11:04 +0000

    RyoikiTenkai

---
+++ H+ Sport.session.sql
@@ -0,0 +1,7 @@
+Create Table JujutsuHigh (
+  StudentID int (10) Not Null,
+  SorcererType nvarchar (10) Not Null,
+  FirstName nvarchar(50) Not Null,
+  LastName nvarchar(50) Not Null,
+  City nvarchar(50) Not Null
+)
